470735ecad105a9831b06e3f83199c4565093d99
Cristopher-Germuts <cristophergc@hotmail.com>
2021-11-07 15:37:16 +1300

    Sumit test v1

--- README.md
+++ README.md
@@ -1,28 +1,17 @@
-# A starter webpack project for React
+JavaScript Code Testing - Callinf an API 
+(using EDA boilerplate for React applications)
 
-This is a starter project that uses webpack to transpile and bundle ES6 React code. To use, consider these steps:
+Clone Repository to local machine
 
-* Fork this repo
-* Rename your repo according to the app you're building
 
-```sh
-git clone https://github.com/[your-account]/[your-app].git
-cd [your-app] && npm i
-```
+At the Terminal:
+`npm install` to install dependencies
 
-To start the development server with a watcher that rebuilds your code, run `npm run dev`. The assets built by webpack are placed in `server/public`. This folder is defined as a static folder in an Express.js server that can be started with `npm run server`.
 
-Additional components should be placed in `client/components`.
+At the Terminal:
+Run `npm run dev` to start the application.
 
-## Separate client/server
 
-The boilerplate is also set up to host the client using `webpack-dev-server` with hot module reloading etc. To use this method, in one terminal run:
-```sh
-npm run client
-```
-and in the other:
-```sh
-npm run server
-```
-The client will be available on http://localhost:8080 and the server on http://localhost:3000. Note that you will still need to manage CORS between the two, as they are on different ports.
+Open a browser and type http://localhost:3000/
 
+Enjoy
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,24 +1,96 @@
-import React, {useState, useEffect} from 'react'
-import {getGreeting} from '../apiClient'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Form, Input, Button, FormGroup, Container, Row, Col } from 'reactstrap'
 
 const App = () => {
+  const [emailVal, setEmailVal] = useState('')
+  const [secretVal, setSecretVal] = useState('')
+  const [id, setId] = useState('')
+  const [repoURL, setRepoURL] = useState('')
+  const [modified, setModified] = useState('')
+  const [showResponse, setShowResponse] = useState(false)
 
-  const [greeting, setGreeting] = useState('')
-  const [count, setCount] = useState(0)
+  function getUserDetails (e) {
+    e.preventDefault()
 
-  useEffect(() => {
-    getGreeting()
-      .then((greeting) => {
-        console.log(greeting)
-        setGreeting(greeting)
-      })
-  }, [count])
+    axios.get(
+      `https://tweakplan.com/JavaScriptDemoSubmission-1.0/candidates?email=${emailVal}&secret=${secretVal}`
+    ).then((result) => {
+      setId(result.data.id)
+      setRepoURL(result.data.repoURL)
+      setModified(result.data.modified)
+      setShowResponse(true)
+      return null
+    }).catch((err) => {
+      console.log(err)
+    })
+
+    // const response = sendUser(form)
+  }
+  function submitNewURL (e) {
+    e.preventDefault()
+    axios.patch(
+      `https://tweakplan.com/JavaScriptDemoSubmission-1.0/candidates/${id}`,
+      { secret: secretVal, repoURL: repoURL }
+    ).then((result) => {
+      setId(result.data.id)
+      setRepoURL(result.data.repoURL)
+      setModified(result.data.modified)
+      setShowResponse(true)
+      return null
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+  function handleChange (e) {
+    setEmailVal(e.target.value)
+    setSecretVal(e.target.value)
+  }
+
+  function handleRepoUrlChange (e) {
+    setRepoURL(e.target.value)
+  }
 
   return (
     <>
-    {count}
-    <h1>{greeting}</h1>
-    <button onClick={() => setCount(count + 1)}>Click</button>
+      <Container>
+        <Row>
+          <Col>
+            <Form>
+              <h1>Cristopher Germuts</h1>
+              <FormGroup className="mb-2">
+                <Input type= "text" name="email" onChange={handleChange} placeholder="Enter here" className="form-control" />
+              </FormGroup>
+              <FormGroup className="mb-2">
+                <Input type= "text" name="secret" onChange={handleChange} placeholder="Enter here" className="form-control" />
+              </FormGroup>
+              <Button onClick={getUserDetails}>Click to get your user details</Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+
+      {showResponse
+        ? <div>
+          {
+            id
+              ? <div>
+                <div>Your Id: {id}</div>
+                <div>Your github URL: {repoURL}</div>
+                <div>Modified: {modified}</div>
+                <form>
+                  <div>
+                    <input type= "text" name="repoURL" onChange={handleRepoUrlChange} placeholder="Enter here" ></input>
+                  </div>
+                  <button onClick={submitNewURL}>Click to update your github URL</button>
+                </form>
+              </div>
+              : <div>Invalid user</div>
+          }
+
+        </div>
+        : ''
+      }
     </>
   )
 }
